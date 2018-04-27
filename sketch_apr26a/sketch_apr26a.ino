736f16d6b5d07a6e11c33e529444b8e63d1a3ac5
rocioGomez <y.aoi.kinomoto@gmail.com>
2018-04-27 07:07:12 -0300

    Comandos entrada

--- sketch_apr26a/sketch_apr26a.ino
+++ sketch_apr26a/sketch_apr26a.ino
@@ -17,7 +17,74 @@ void setup() {
 }
 
 void loop() {
-
+// put your main code here, to run repeatedly:
+        // send data only when you receive data:
+        /* 1 y 2: aumentar y disminuir 5 grados base 
+         * 3 y 4: aumentar y disminuir 5 grados vertical 
+         * 5 y 6: aumentar y disminuir 5 grados horizontal
+         * 7 y 8: aumentar y disminuir 5 grados pinza 
+        */
+        base = servoB.read();
+        hori = servoH.read();
+        verti = servoV.read();
+        pinz = servoP.read();
+        if (Serial.available() >0) {
+                // read the incoming byte:
+                entrada = Serial.read();                
+                
+                if (entrada == 49) {
+                // mas 5 base
+                mover_Base(base+10);
+                Serial.print("B: ");
+                Serial.println(servoB.read());
+              }
+              
+                if (entrada == 50) {
+                // mover mas 5 grados base
+                mover_Base(base-10);
+                Serial.print("B: ");
+                Serial.println(servoB.read());
+              }
+              
+                if (entrada == 52) {
+                // mas 5 vertical
+                mover_Vertical(verti+15);
+                Serial.print("V: ");
+                Serial.println(servoV.read());
+              }
+                if (entrada == 51) {
+                // -5  vertical
+                mover_Vertical(verti-15);
+                Serial.print("V: ");
+                Serial.println(servoV.read());
+              }
+                if (entrada == 53) {
+                // +5 horizontal
+                mover_Horizontal(hori+10);
+                Serial.print("H: ");
+                Serial.println(servoH.read());
+              }
+                if (entrada == 54) {
+                // -5 horizontal
+                mover_Horizontal(hori-10);
+                Serial.print("H: ");
+                Serial.println(servoH.read());
+              }
+                if (entrada == 55) {
+                //+5 pinza
+                mover_Pinza(pinz+10);
+                Serial.print("P: ");
+                Serial.println(servoP.read());
+              }
+                if (entrada == 56) {
+                // -5 pinza
+                mover_Pinza(pinz-10);
+                Serial.print("P: ");
+                Serial.println(servoP.read());
+              }
+              
+        }
+             
              
 }
 
